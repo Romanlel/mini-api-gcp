ab68f1306c7ca8277bf461117a5256c259d5bcf4
Romanlel <roman.lacaze]gmail.com>
2025-05-20 13:18:26 +0200

    Supprime les fichiers

--- app/joke_utils.py
+++
@@ -1,7 +0,0 @@
-from google.cloud import aiplatform
-
-def generate_joke():
-    aiplatform.init(project="YOUR_PROJECT_ID", location="us-central1")
-    prompt = "Raconte une blague originale et marrante."
-    response = aiplatform.TextGenerationModel.from_pretrained("text-bison").predict(prompt=prompt)
-    return response.text.strip()
